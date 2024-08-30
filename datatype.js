d6026bcb486cf279c15cd0358f97a0bf0be29c94
Waneezashafiq <127325358+Waneezashafiq@users.noreply.github.com>
2024-08-30 15:41:03 +0500

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,7 @@
+let Name="Waneeza";
+let age=18;
+let a=null;
+let value=Symbol('hello');
+let student={name:"Waneeza"};
+
+console.table([typeof Name, typeof age, typeof a, typeof value, typeof student]);
